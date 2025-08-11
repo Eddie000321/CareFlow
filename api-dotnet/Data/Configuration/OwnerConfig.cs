21f6a02cfd97cc7df62e4d14612c1e31fe93a248
Eddie <Eddie000321@outlook.com>
2025-08-11 09:07:57 -0400

    add Ownerconfig.cs make Property and ComplexProperty about Address(country,Province,City,Street,PostalCode)

--- api-dotnet/Data/Configuration/OwnerConfig.cs
+++ api-dotnet/Data/Configuration/OwnerConfig.cs
@@ -8,12 +8,28 @@ public class OwnerConfig : IEntityTypeConfiguration<Owner> // Connect with CareF
     {
         mb.HasKey(o => o.Id);
 
-        mb.HasIndex(o => new { o.Name, o.Phone });
-
         // Property : Configures a single scalar property (column) of your entity.
         // ComplexProperty(EF Core8+ Only) :Configures a nested value object inside your entity, not a simple scalar.
-        mb.Property();
+        mb.Property(o => o.Name).IsRequired().HasMaxLength(100);
+        mb.Property(o => o.Phone).IsRequired().HasMaxLength(30);
+        mb.Property(o => o.Email).HasMaxLength(254);
 
+        mb.HasIndex(o => new { o.Name, o.Phone });
 
+        mb.ComplexProperty(o => o.OwnerAddress, a =>
+        {
+            a.Property(p => p.Country).IsRequired().HasMaxLength(2).HasColumnName("Country");
+            a.Property(p => p.Province).IsRequired().HasMaxLength(2).HasColumnName("Province");
+            a.Property(p => p.City).IsRequired().HasMaxLength(100).HasColumnName("City");
+            a.Property(p => p.Street).IsRequired().HasMaxLength(200).HasColumnName("Street");
+            a.Property(p => p.PostalCode).IsRequired().HasMaxLength(10).HasColumnName("Postal Code");
+        });
+
+        // For search
+        mb.HasIndex(o => EF.Property<String>(o, "City"));
+        mb.HasIndex(o => EF.Property<String>(o, "Province"));
+        mb.HasIndex(o => EF.Property<String>(o, "PostalCode"));
+
+        mb.HasMany(o => o.Pets).WithOne(p => p.Owner).HasForeignKey(p => p.OwnerId).OnDelete(DeleteBehavior.Cascade);
     }
 }
--- api-dotnet/Domain/Owner.cs
+++ api-dotnet/Domain/Owner.cs
@@ -17,9 +17,11 @@ public class Owner
 
     [ComplexType] // EF Core 8+
     public class Address
-    {
-        [MaxLength(200)] public string Street { get; set; } = "";
-        [MaxLength(100)] public string City { get; set; } = "";
-        [MaxLength(20)]  public string PostalCode { get; set; } = "";
-    }
+{
+    [MaxLength(2)]   public string Country { get; set; } = "CA"; // ISO-2
+    [MaxLength(2)]   public string Province { get; set; } = "ON"; // e.g., ON
+    [MaxLength(100)] public string City { get; set; } = "";
+    [MaxLength(200)] public string Street { get; set; } = "";
+    [MaxLength(10)]  public string PostalCode { get; set; } = "";
+}
 }
